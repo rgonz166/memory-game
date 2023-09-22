16a85f2147614381e372781fe4baebdadc041542
Raul Gonzalez <rgonz166@gmail.com>
2023-09-22 16:54:58 -0700

    Create PWA badge

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import shuffle from "./utilities/shuffle";
 import Card from "./components/Card";
 import Header from "./components/Header";
+import useAppBadge from "./hooks/useAppBadge";
 
 function App() {
   const [cards, setCards] = useState(shuffle);
@@ -9,6 +10,7 @@ function App() {
   const [pickTwo, setPickTwo] = useState(null);
   const [disabled, setDisabled] = useState(false);
   const [wins, setWins] = useState(0);
+  const [setBadge, clearBadge] = useAppBadge();
 
   const handleClick = (card) => {
     if (!disabled) {
@@ -23,6 +25,7 @@ function App() {
   };
 
   const handleNewGame = () => {
+    clearBadge();
     setWins(0);
     handleTurn();
     setCards(shuffle);
@@ -71,9 +74,10 @@ function App() {
       console.log("You win!");
       setWins(wins + 1);
       handleTurn();
+      setBadge();
       setCards(shuffle);
     }
-  }, [cards, wins]);
+  }, [cards, wins, setBadge]);
 
   return (
     <>
