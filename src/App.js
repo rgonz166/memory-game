871067db32a49986c5dac453afab7f82baa9f13a
Raul Gonzalez <rgonz166@gmail.com>
2023-09-22 16:22:43 -0700

    Add game logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,72 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import shuffle from "./utilities/shuffle";
 import Card from "./components/Card";
 
 function App() {
   const [cards, setCards] = useState(shuffle);
+  const [pickOne, setPickOne] = useState(null);
+  const [pickTwo, setPickTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const [wins, setWins] = useState(0);
+
+  const handleClick = (card) => {
+    if (!disabled) {
+      pickOne ? setPickTwo(card) : setPickOne(card);
+    }
+  };
+
+  const handleTurn = () => {
+    setPickOne(null);
+    setPickTwo(null);
+    setDisabled(false);
+  };
+
+  useEffect(() => {
+    let pickTimer;
+
+    // Two cards have been click
+    if (pickOne && pickTwo) {
+      // Check if the cards are the same
+      if (pickOne.image === pickTwo.image) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.image === pickOne.image) {
+              // Update card property to reflect match
+              return { ...card, matched: true };
+            } else {
+              // No Match
+              return card;
+            }
+          });
+        });
+        handleTurn();
+      } else {
+        // Prevent new selections until after delay
+        setDisabled(true);
+        // Add delay between selections
+        pickTimer = setTimeout(() => {
+          handleTurn();
+        }, 1000);
+      }
+    }
+    return () => {
+      clearTimeout(pickTimer);
+    };
+  }, [cards, pickOne, pickTwo]);
+
+  // If player has found all matches, handle accordingly
+  useEffect(() => {
+    // Check for any remaining card matches
+    const checkWin = cards.filter((card) => !card.matched);
+
+    // All matches made, handle win/badge counters
+    if (cards.length && checkWin.length < 1) {
+      console.log("You win!");
+      setWins(wins + 1);
+      handleTurn();
+      setCards(shuffle);
+    }
+  }, [cards, wins]);
 
   return (
     <>
@@ -11,7 +74,12 @@ function App() {
         {cards.map((card) => {
           const { image, id, matched } = card;
           return (
-            <Card key={id} image={image} selected={false} onClick={() => {}} />
+            <Card
+              key={id}
+              image={image}
+              selected={card === pickOne || card === pickTwo || matched}
+              onClick={() => handleClick(card)}
+            />
           );
         })}
       </div>
