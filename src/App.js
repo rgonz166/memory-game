ca667f130b9c9d1d0b8be206ba3eee44745dc3b0
Raul Gonzalez <rgonz166@gmail.com>
2023-09-22 15:57:02 -0700

    Add card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import shuffle from "./utilities/shuffle";
+import Card from "./components/Card";
 
 function App() {
   const [cards, setCards] = useState(shuffle);
 
   return (
     <>
-      <div className="grid"></div>
+      <div className="grid">
+        {cards.map((card) => {
+          const { image, id, matched } = card;
+          return (
+            <Card key={id} image={image} selected={false} onClick={() => {}} />
+          );
+        })}
+      </div>
     </>
   );
 }
